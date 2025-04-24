efa7c401c6e0f9c1672f04a84f2ca984ab26e68d
suzuki <kumanojudoka@gmail.com>
2025-04-24 12:04:37 +0900

    Chapter7

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -56,5 +56,6 @@ public class EnemyController : MonoBehaviour
         broken = false;
         rb.simulated = false;   
         anim.SetTrigger("Fixed");
+        transform.GetChild(0).GetComponent<ParticleSystem>().Stop();
     }
 }
